7ff66ff234c57537e949903ee60157ad356c1b8d
Lindelwa Twala <lindelwa@kpims.co.za>
2026-09-06 20:46:03 +0200

    first commit

---
+++ personal_training.py
@@ -0,0 +1,16 @@
+# Data types
+name = "Linda"                                      # str
+age = 26                                            # int
+weight = 63.5                                       # float
+likes = ["food", "exercise", "netflix"]             # list
+
+print(name)
+print(age)
+print(weight)
+print(likes)
+
+# Test
+x = 9
+b = 3
+a = x > b
+print(a)
